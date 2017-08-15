0b166ca3c4767a0b70b59cdc3d215cc61841b3a7
logonrm <logonrm@fiap.com.br>
2017-08-14 21:09:31 -0300

    Adding linecast

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -6,6 +6,9 @@ public class Personagem : MonoBehaviour {
 
 	public float velocidade;
 	public float impulso;
+	public Transform verificaChao;
+	bool estaNoChao;
+	bool puloDuplo;
 
 	Rigidbody2D rb;
 
@@ -18,12 +21,22 @@ public class Personagem : MonoBehaviour {
 	void Update () {
 
 		// Pulo
-		if (Input.GetButtonDown("Jump")) {
+		if (Input.GetButtonDown("Jump") && estaNoChao) {
 			rb.AddForce (Vector2.up * impulso);
+			puloDuplo = true;
+		} else if (Input.GetButtonDown("Jump") && !estaNoChao && puloDuplo) {
+			rb.AddForce (Vector2.up * impulso);
+			puloDuplo = false;
 		}
 
 		// Movimento
 		float mover = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
 		transform.Translate (mover, 0.0f, 0.0f);
+
+		// Verifica colisão com os blocos do layer Piso
+		estaNoChao = Physics2D.Linecast(transform.position, 
+			verificaChao.position, 1 << LayerMask.NameToLayer("Piso"));
+
+		print (estaNoChao);
 	}
 }
